1cd104b46634622e143594044a2aac9b14e28aa9
baidongqi <dbai1@hawk.iit.edu>
2024-03-06 20:56:30 -0600

    feat: fix the update the data

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Address Book home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Address Book' });
+  res.render('index', { title: 'Address Book (SQLite)' });
 });
 
 module.exports = router;
